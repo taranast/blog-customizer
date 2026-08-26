d0c0d1445a500abafcd6553fe507491a038d25cf
taranast <taranast434@gmail.com>
2026-08-26 19:43:14 +0300

    feat(ui): add selectors to form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,6 +1,17 @@
 import { useRef, useState } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 import clsx from 'clsx';
@@ -8,6 +19,11 @@ import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [fontFamily, setFontFamily] = useState(fontFamilyOptions[0]);
+	const [fontColor, setFontColor] = useState(fontColors[0]);
+	const [backgroundColor, setBackgroundColor] = useState(backgroundColors[0]);
+	const [contentWidth, setContentWidth] = useState(contentWidthArr[0]);
+	const [fontSize, setFontSize] = useState(fontSizeOptions[0]);
 	const rootRef = useRef<HTMLDivElement>(null);
 
 	useOutsideClickClose({
@@ -27,6 +43,41 @@ export const ArticleParamsForm = () => {
 			<aside
 				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={fontFamily}
+						onChange={setFontFamily}
+						options={fontFamilyOptions}
+						title='шрифт'
+					/>
+					<RadioGroup
+						selected={fontSize}
+						name='radio'
+						onChange={setFontSize}
+						options={fontSizeOptions}
+						title='размер шрифта'
+					/>
+					<Select
+						selected={fontColor}
+						onChange={setFontColor}
+						options={fontColors}
+						title='цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={backgroundColor}
+						onChange={setBackgroundColor}
+						options={backgroundColors}
+						title='цвет фона'
+					/>
+					<Select
+						selected={contentWidth}
+						onChange={setContentWidth}
+						options={contentWidthArr}
+						title='ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
